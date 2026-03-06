e888afa2bbb28622d61c71d4b8b5314e6add9679
Gaurav Bawa <gauravbawa0609@gmail.com>
2026-03-06 07:34:46 +0530

    add schema types with sanity and sample-data with the config of sanity live

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,12 +2,14 @@ import { ClerkProvider } from '@clerk/nextjs'
 
 import { TooltipProvider } from '@/components/ui/tooltip'
 import { Toaster } from '@/components/ui/sonner'
+import { SanityLive } from '@/sanity/lib/live'
 
 export default function AppLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <ClerkProvider>
       <main>
         <TooltipProvider>{children}</TooltipProvider>
+        <SanityLive />
         <Toaster />
       </main>
     </ClerkProvider>
--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,10 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
+import { categoryType } from './category_type'
+import { customerType } from './customer_type'
+import { productType } from './product_type'
+import { orderType } from './order_type'
+
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [categoryType, customerType, productType, orderType],
 }
